af9cc0b30961243c5793245c446e7742549ef8db
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2019-04-05 12:21:20 +0200

    excludes init/ folder from bootstrap rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".macos" \
+		--exclude "init/" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
